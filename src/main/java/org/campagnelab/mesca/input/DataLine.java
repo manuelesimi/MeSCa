647ff04ff5544fcde6116bd2f7b69bfee3c96823
Manuele Simi <manuele.simi@campagnelab.org>
2013-12-28 17:26:50 -0500

    Add DataLine to model data extracted from each line in the VCF file.

---
+++ src/main/java/org/campagnelab/mesca/input/DataLine.java
@@ -0,0 +1,17 @@
+package org.campagnelab.mesca.input;
+
+/**
+ * Data extracted from a line read in the VCF file.
+ *
+ * @author manuele
+ */
+public class DataLine {
+
+    /**
+     * Parses and extracts data from a line read in the VCF file
+     * @param line
+     */
+    protected DataLine(String line) {
+
+    }
+}
