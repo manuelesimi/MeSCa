1ba3918fff1cc81054b0911db5d8d56a54879fdf
Manuele Simi <manuele.simi@campagnelab.org>
2014-01-08 12:55:16 -0500

    Preliminary work on cluster detection.

--- src/main/java/org/campagnelab/mesca/algorithm/ClusterDetector.java
+++ src/main/java/org/campagnelab/mesca/algorithm/ClusterDetector.java
@@ -1,22 +1,47 @@
 package org.campagnelab.mesca.algorithm;
 
+import org.campagnelab.mesca.input.Sample;
+import org.campagnelab.mesca.list.DoublyLinkedList;
+
 import java.util.SortedSet;
 import java.util.TreeSet;
 
 /**
  * The cluster detection method.
- *
+ * <p/>
  * Given a list of variations in a genome, it clusters them with an adaptive
- * quality-conscious approach.
+ * quality-conscious approach according to stop conditions injected in the method.
  *
  * @author manuele
  */
 public final class ClusterDetector {
 
-   SortedSet<StopCondition> stopConditions = new TreeSet<StopCondition>();
+    private SortedSet<StopCondition> stopConditions = new TreeSet<StopCondition>();
 
-   public void addStopCondition(StopCondition stopCondition) {
-       this.stopConditions.add(stopCondition);
-   }
+    private final DoublyLinkedList<Sample> sampleList;
 
+
+    /**
+     * @param sampleList the list of samples to clusterize.
+     */
+    public ClusterDetector(DoublyLinkedList<Sample> sampleList) {
+        this.sampleList = sampleList;
+    }
+
+    public void addStopCondition(StopCondition stopCondition) {
+        this.stopConditions.add(stopCondition);
+    }
+
+    /**
+     * Detects the cluster in the sampleList.
+     * @return
+     */
+    public ClusterQueue detect() {
+        ClusterQueue clusters = new ClusterQueue();
+        for (Sample sample : sampleList) {
+            //try to build a cluster around the sample
+
+        }
+        return clusters;
+    }
 }
---
+++ src/main/java/org/campagnelab/mesca/algorithm/ClusterQueue.java
@@ -0,0 +1,22 @@
+package org.campagnelab.mesca.algorithm;
+
+/**
+ * The clusters detected by {@link org.campagnelab.mesca.algorithm.ClusterDetector  }
+ *
+ * @author manuele
+ */
+public class ClusterQueue {
+
+    /**
+     * Add a new cluster.
+     *
+     * @param cluster
+     */
+    protected void addCluster(Cluster cluster) {
+
+    }
+
+    public int size() {
+        return 0;
+    }
+}
