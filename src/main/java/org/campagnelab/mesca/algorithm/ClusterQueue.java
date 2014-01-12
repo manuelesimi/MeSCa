8bf0f7c7cb8b38b6f72b76d7484f4ee2bda7344f
Manuele Simi <manuele.simi@campagnelab.org>
2014-01-12 12:33:09 -0500

    Model the output cluster queue.

--- src/main/java/org/campagnelab/mesca/algorithm/ClusterQueue.java
+++ src/main/java/org/campagnelab/mesca/algorithm/ClusterQueue.java
@@ -1,22 +1,49 @@
 package org.campagnelab.mesca.algorithm;
 
+import it.unimi.dsi.fastutil.objects.ObjectArrayPriorityQueue;
+
+import java.util.Comparator;
+
 /**
- * The clusters detected by {@link org.campagnelab.mesca.algorithm.ClusterDetector  }
+ * The clusters detected by {@link org.campagnelab.mesca.algorithm.ClusterDetector}.
+ * Cluster are queued and dequeued according to the given comparator.
  *
  * @author manuele
  */
 public class ClusterQueue {
 
+    private ObjectArrayPriorityQueue<Cluster> outputQueue;
+
     /**
-     * Add a new cluster.
+     *
+     * @param comparator the comparator used in this queue
+     */
+    protected ClusterQueue(Comparator<? super Cluster> comparator) {
+        outputQueue = new ObjectArrayPriorityQueue<Cluster>(comparator);
+    }
+
+    /**
+     * Enqueues a new cluster.
      *
      * @param cluster
      */
     protected void addCluster(Cluster cluster) {
-
+       outputQueue.enqueue(cluster);
     }
 
+    /**
+     * Dequeues the first cluster from the queue.
+     * @return
+     */
+    public Cluster getCluster() {
+        return outputQueue.dequeue();
+    }
+
+    /**
+     * Returns the number of clusters in this queue.
+     * @return
+     */
     public int size() {
-        return 0;
+        return outputQueue.size();
     }
 }
